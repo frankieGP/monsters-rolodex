b280e4bdcef7d3ab45c83122fba5c4370d2fab9b
Frankie Gavin <frankanthonygavin@gmail.com>
2019-11-06 13:16:52 -0800

    adds an mapped array of names and ids

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor() {
+  super();
+
+  this.state = {
+    monsters: [
+      {
+        name: 'The Shape',
+        id: '1'
+
+      },
+      {
+        name: 'Dracula',
+        id: '2'
+
+      },
+      {
+        name: 'Wolfman',
+        id: '3'
+
+      }
+    ] 
+  };
+}
+
+
+render() {
+    return (
+      <div className='App'>
+      {
+        this.state.monsters.map(monster => 
+          <h1 key={monster.id}>{ monster.name }</h1>)
+      }
+      </div>
+    )
+  }
+}
+
+export default App;
