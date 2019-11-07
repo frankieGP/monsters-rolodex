f3cde51298043fa2bdd2df635dc9caf079710390
Frankie Gavin <frankanthonygavin@gmail.com>
2019-11-07 11:27:55 -0800

    creates a function component that takes children of the props and CSS  to create a styled card containing the monster relavent props

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -21,12 +21,9 @@ componentDidMount() {
 render() {
     return (
       <div className='App'>
-      <CardList>
-      {
-        this.state.monsters.map(monster => 
-          <h1 key={monster.id}>{ monster.name }</h1>)
-      }
-      </CardList>
+      <CardList monsters={this.state.monsters}/>
+
+      
       </div>
     )
   }
