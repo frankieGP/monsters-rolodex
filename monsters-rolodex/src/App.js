09771b05ba793e5b9ec27568717c8c137da879cd
Frankie Gavin <frankanthonygavin@gmail.com>
2019-11-19 13:28:24 -0800

    used cp -r to fix nested folder, added css, added homepage and deploy scripts

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
-import { SearchBox } from './components/search-box/search-box.component'
+import { SearchBox } from './components/search-box/search-box.component';
 
 
 
@@ -31,6 +31,7 @@ render() {
   );
     return (
       <div className='App'>
+      <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters' 
           handleChange={ e => 
